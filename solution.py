cb519cd7b402cc0097bd8dcecba4ccddf4628552
Evgeny Nikolaev <50527841+kainev11@users.noreply.github.com>
2023-04-08 03:05:53 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 294776608 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    mu = np.log(391) 
+    sigma = np.sqrt(np.log(1 + (b / 391) ** 2))
+    log_x = np.log(x)
+    log_x_mean = log_x.mean()
+    a_estimate = np.exp(log_x_mean - mu) 
+    return a_estimate
